6d028a5bf91aa7da7f91bbaae31548de47dbbd6b
RaquelOliveira <ods.raquel@gmail.com>
2015-11-08 23:53:51 -0300

    MongoDB
    Conexão com o mongoDB e criação da tabela de Usuarios

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var http = require('http');
+//var http = require('http');
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 var load = require('express-load');
+var mongoose = require('mongoose');
 //var user = require('./routes/users');
 //var routes = require('./routes');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+mongoose.connect('mongodb://localhost/projetoExpress', function(err){
+	if (err) {
+		console.log('Erro ao conectar no mongodb'+err);
+	}
+});
+
+//app.set('port', process.env.PORT || 3000);
+//app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -16,16 +24,19 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
 load('models').then('controllers').then('routes').into(app);  
-//app.get('/', routes.index);
-//app.get('/user', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server rodando na porta ' + app.get('port'));
+app.listen(3000,function(){
+	console.log('Servisor rodando na porta 3000 ...');
 });
+
+/*http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server rodando na porta ' + app.get('port'));
+});*/
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,37 @@
 module.exports = function(app) {
+
+	var Usuario = app.models.usuarios;
+
 	var UsuarioController = {
 		index: function(req, res){
 			res.render("usuarios/index");
+		},
+
+		create: function(req,res){
+			var model = new Usuario({
+				nome:'Raquel',
+				login: 'raquel',
+				senha: '123456'
+			});
+
+			model.save(function(erro,data){
+				if(erro){
+					console.log(erro);
+				}else{
+					res.json(data);
+				}
+			})
+		},
+
+		lista : function(req,res){
+			Usuario.find(function(erro,data){
+				if(erro){
+					console.log(erro);
+				}else{
+					res.json(data);
+				}
+
+			});
 		}
 	}
 	return UsuarioController;
